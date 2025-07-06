9ec14c1458396a62367c61beaad5b4247e0d1395
Sean Mafnas <seanvincent671@gmail.com>
2025-07-06 20:50:53 +1000

    Refactor index.html to use anchor tags for navigation buttons and update script.js to comment out unused code; adjust style.css for improved layout and button styling

--- script.js
+++ script.js
@@ -1,24 +1,24 @@
-const buttonElements = document.querySelectorAll(".btn");
-const frameElement = document.getElementById("challenge-frame");
-const button2Elements = document.querySelectorAll(".btn2");
+// const buttonElements = document.querySelectorAll(".btn");
+// const frameElement = document.getElementById("challenge-frame");
+// const button2Elements = document.querySelectorAll(".btn2");
 
-const handleClick = (e) => {
-  const clickedButtonEl = e.currentTarget;
-  let url = "";
-  if (clickedButtonEl.classList.contains("btn-1")) {
-    url = "./challenge-1/challenge1.html";
-  } else if (clickedButtonEl.classList.contains("btn-2")) {
-    url = "./challenge-2/challenge2.html";
-  } else if (clickedButtonEl.classList.contains("btn-3")) {
-    url = "./challenge-3/challenge3.html";
-  } else if (clickedButtonEl.classList.contains("btn-4")) {
-    url = "./challenge-4/challenge4.html";
-  }
+// const handleClick = (e) => {
+//   const clickedButtonEl = e.currentTarget;
+//   let url = "";
+//   if (clickedButtonEl.classList.contains("btn-1")) {
+//     url = "./challenge-1/challenge1.html";
+//   } else if (clickedButtonEl.classList.contains("btn-2")) {
+//     url = "./challenge-2/challenge2.html";
+//   } else if (clickedButtonEl.classList.contains("btn-3")) {
+//     url = "./challenge-3/challenge3.html";
+//   } else if (clickedButtonEl.classList.contains("btn-4")) {
+//     url = "./challenge-4/challenge4.html";
+//   }
 
-  if (url && frameElement) {
-    frameElement.src = url;
-    frameElement.style.display = "block";
-  }
-};
+//   if (url && frameElement) {
+//     frameElement.src = url;
+//     frameElement.style.display = "block";
+//   }
+// };
 
-buttonElements.forEach((btn) => btn.addEventListener("click", handleClick));
+// buttonElements.forEach((btn) => btn.addEventListener("click", handleClick));
